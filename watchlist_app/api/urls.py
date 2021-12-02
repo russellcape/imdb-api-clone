2c8d3f76b1da9d146396b550c87f643e61f24e7a
russellcape <russellcape98@gmail.com>
2021-12-02 15:20:37 -0500

    Updated paths by using new generics api views

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
+from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail, ReviewCreate
 
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watchlist'),
-    path('<int:pk>', WatchDetailAV.as_view(), name='watchlist-detail'),
+    path('<int:pk>/', WatchDetailAV.as_view(), name='watchlist-detail'),
 
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
-    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
 
-    path('review/', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    # path('review/', ReviewList.as_view(), name='review-list'),
+    # path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 
-    # path('stream/<int:pk>/reveiw>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
-    # path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    path('stream/<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create'),
+    path('stream/<int:pk>/review/', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
 ]
