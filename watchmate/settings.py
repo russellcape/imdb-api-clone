3d42afd29e67aec204bbd2c580d998615506aa5d
russellcape <russellcape98@gmail.com>
2021-12-03 15:35:12 -0500

    commented out simple jwt in settings

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -141,8 +141,8 @@ REST_FRAMEWORK = {
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
 
